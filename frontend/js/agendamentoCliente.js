97241bccfb2727203369028829b8818e6102b4c5
Carla Cristina <carlacristina130523@gmail.com>
2026-04-29 20:05:30 -0300

    feat(js): adicionar mock estatico do banco de dados de servicos

--- frontend/js/agendamentoCliente.js
+++ frontend/js/agendamentoCliente.js
@@ -1,3 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     
+    // 1. DADOS FALSOS SIMULANDO O BACK-END
+    const servicosDoBancoDeDados = [
+        { id: 101, nome: "Corte de Cabelo Feminino", duracao: "60 minutos", valor: 150.00 },
+        { id: 102, nome: "Escova Progressiva", duracao: "120 minutos", valor: 350.00 },
+        { id: 103, nome: "Manicure Especializada", duracao: "45 minutos", valor: 80.00 },
+        { id: 104, nome: "Pedicure Premium", duracao: "45 minutos", valor: 80.00 },
+        { id: 105, nome: "Massagem Capilar Relaxante", duracao: "30 minutos", valor: 120.00 },
+        { id: 106, nome: "Coloração Completa", duracao: "180 minutos", valor: 450.00 }
+        { id: 101, nome: "Corte de Cabelo Feminino", duracao: "60 minutos", valor: 150.00 },
+        { id: 102, nome: "Escova Progressiva", duracao: "120 minutos", valor: 350.00 },
+        { id: 103, nome: "Manicure Especializada", duracao: "45 minutos", valor: 80.00 },
+        { id: 104, nome: "Pedicure Premium", duracao: "45 minutos", valor: 80.00 },
+        { id: 105, nome: "Massagem Capilar Relaxante", duracao: "30 minutos", valor: 120.00 },
+        { id: 106, nome: "Coloração Completa", duracao: "180 minutos", valor: 450.00 }
+    ];
+
 });
