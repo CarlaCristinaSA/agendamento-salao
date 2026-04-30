3a5892c14487a7383377ef9ba607e754919c3820
Carla Cristina <carlacristina130523@gmail.com>
2026-04-29 22:50:43 -0300

    feat(js): inicializar constantes de datas e horarios

---
+++ frontend/js/modalAgendarHorario.js
@@ -0,0 +1,15 @@
+const PT_DAYS   = ['DOM','SEG','TER','QUA','QUI','SEX','SÁB'];
+const PT_MONTHS = [
+    'Janeiro','Fevereiro','Março','Abril','Maio','Junho',
+    'Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'
+];
+const PT_MONTHS_SHORT = [
+    'Jan','Fev','Mar','Abr','Mai','Jun',
+    'Jul','Ago','Set','Out','Nov','Dez'
+];
+
+const ALL_TIMES = [
+    '09:00','09:30','10:00','10:30','11:00','11:30',
+    '14:00','14:30','15:00','15:30','16:00','16:30',
+    '17:00','17:30'
+];
