47b35aa75ce40285e5530ac1815754577209a85c
EmillyW <emillywpinheiro@gmail.com>
2026-05-01 00:35:14 -0300

    feat: adicionar modal alterar horários da semana

--- frontend/js/admin-horarios.js
+++ frontend/js/admin-horarios.js
@@ -1,58 +1,119 @@
-//Ajusta o número do dia no card de acordo com a semana atual
-const elementosNumeroDia = document.querySelectorAll('.numero-dia');
-const dataDeHoje = new Date();
-const domingoDestaSemana = new Date(dataDeHoje);
-domingoDestaSemana.setDate(dataDeHoje.getDate() - dataDeHoje.getDay()); 
+/*Gerar os cards de dia e colocar os dias corretos da semana atual*/
+const diasSemana = ["DOMINGO", "SEGUNDA", "TERÇA", "QUARTA", "QUINTA", "SEXTA", "SÁBADO"];
 
-elementosNumeroDia.forEach((elemento, index) => {
-    const dataDoCartao = new Date(domingoDestaSemana);
-    dataDoCartao.setDate(domingoDestaSemana.getDate() + index);
-    elemento.textContent = dataDoCartao.getDate();
-});
+function renderizarDiasDaSemana() {
+    const containerDias = document.getElementById('container-dias-semana');
+    const dataDeHoje = new Date();
+    const domingoDestaSemana = new Date(dataDeHoje);
+    domingoDestaSemana.setDate(dataDeHoje.getDate() - dataDeHoje.getDay());
 
-/*Ações para o modal de editar horário padrão*/
-document.addEventListener('DOMContentLoaded', () => {
-
-    const btnAbrirModal = document.getElementById('alterar-horario-padrao');
-    const modalPadrao = document.getElementById('modal-horario-padrao');
-    const btnFecharModal = document.querySelector('.fechar-modal');
-    const btnAdicionar = document.getElementById('add');
-    const formulario = document.querySelector('.formulario-horario-padrao form');
-    const listaHorarios = document.getElementById('lista-horarios');
-
-    btnAbrirModal.addEventListener('click', () => {
-        modalPadrao.style.display = 'flex';
-    });
-
-    btnFecharModal.addEventListener('click', () => {
-        modalPadrao.style.display = 'none';
-    });
-
-    modalPadrao.addEventListener('click', (evento) => {
-        if (evento.target === modalPadrao) {
-            modalPadrao.style.display = 'none';
-        }
-    });
-
-    btnAdicionar.addEventListener('click', () => {
-        const novaLinhaHTML = `
-            <div class="input-time">
-                <input type="text" class="input-intervalo" onfocus="(this.type='time')" onblur="(this.type='text')">
-                <img src="../../assets/intervalo-separator-icon.svg" alt="separador">
-                <input type="text" class="input-intervalo" onfocus="(this.type='time')" onblur="(this.type='text')">
-                <button type="button" class="limpar">X</button>
-            </div>
+    let cardsDias = "";
+    for (let i = 0; i < 7; i++) {
+        const dataDoCartao = new Date(domingoDestaSemana);
+        dataDoCartao.setDate(domingoDestaSemana.getDate() + i);
+        
+        const numeroDoDia = dataDoCartao.getDate();
+        const nomeDoDia = diasSemana[i];
+        cardsDias += `
+            <section class="card-dia">
+                <div class="dia">
+                    <span class="nome-dia">${nomeDoDia}</span><br>
+                    <strong class="numero-dia">${numeroDoDia}</strong>
+                </div>
+                <div class="intervalos-dia">
+                    <span class="intervalo">10:00 - 12:00</span>
+                    <span class="intervalo">14:00 - 18:00</span>
+                </div>
+            </section>
         `;
-        listaHorarios.insertAdjacentHTML('beforeend', novaLinhaHTML);
-    });
+    }
+    containerDias.innerHTML = cardsDias;
+}
 
-    formulario.addEventListener('click', (evento) => {
-        if (evento.target.classList.contains('limpar')) {
-            const linhaParaRemover = evento.target.closest('.input-time');
-            if (linhaParaRemover) {
-                linhaParaRemover.remove();
+// Gerar os dias dentro do modal da semana
+function renderizarDiasModalSemana() {
+    const containerListaDias = document.getElementById('lista-dias-modal');
+    if (!containerListaDias) return;
+
+    let dias = "";
+
+    for (let i = 0; i < 7; i++) {
+
+        dias += `
+            <section class="dia-modal">
+                <div class="cabecalho-dia-modal">
+                    <h2>${diasSemana[i]}</h2>
+                    <input type="checkbox" class="toggle" checked>
+                </div>
+                
+                <div class="container-scroll-horarios">
+                    <div class="input-time">
+                        <input type="text" class="input-intervalo" onfocus="(this.type='time')" onblur="(this.type='text')">
+                        <img src="../../assets/intervalo-separator-icon.svg" alt="separador">
+                        <input type="text" class="input-intervalo" onfocus="(this.type='time')" onblur="(this.type='text')">
+                        <button type="button" class="limpar">X</button>
+                    </div>
+                </div>
+                
+                <div class="add-btn">
+                    <img src="../../assets/add-icon.svg" alt="Adicionar">
+                    <p>ADICIONAR HORÁRIO</p>
+                </div>
+            </section>
+        `;
+    }
+
+    containerListaDias.innerHTML = dias;
+}
+
+/*funcionamento dos modais*/
+document.addEventListener('DOMContentLoaded', () => {
+    renderizarDiasDaSemana();
+    renderizarDiasModalSemana();
+
+    // Abrir os Modais
+    const btnAbrirPadrao = document.getElementById('alterar-horario-padrao');
+    const modalPadrao = document.getElementById('modal-horario-padrao');
+    const btnAbrirSemana = document.getElementById('horarios-semana'); 
+    const modalSemana = document.getElementById('modal-horarios-semana');
+    btnAbrirPadrao.addEventListener('click', () => modalPadrao.classList.add('aberto'));
+    btnAbrirSemana.addEventListener('click', () => modalSemana.classList.add('aberto'));
+
+    // Fechar Modais
+    document.addEventListener('click', (evento) => {
+        const elementoClicado = evento.target;
+
+        if (elementoClicado.classList.contains('fechar-modal') || elementoClicado.id === 'fechar-modal') {
+            elementoClicado.closest('.modal').classList.remove('aberto'); 
+        } 
+        else if (elementoClicado.classList.contains('modal')) {
+            elementoClicado.classList.remove('aberto');
+        }
+
+        // Excluir Linha de Horário
+        else if (elementoClicado.classList.contains('limpar')) {
+            const linhaParaRemover = elementoClicado.closest('.input-time');
+            if (linhaParaRemover) linhaParaRemover.remove();
+        }
+        // Adicionar Nova Linha de Horário
+        else if (elementoClicado.closest('.add-btn')) { 
+            const novaLinhaHTML = `
+                <div class="input-time">
+                    <input type="text" class="input-intervalo" onfocus="(this.type='time')" onblur="(this.type='text')">
+                    <img src="../../assets/intervalo-separator-icon.svg" alt="separador">
+                    <input type="text" class="input-intervalo" onfocus="(this.type='time')" onblur="(this.type='text')">
+                    <button type="button" class="limpar">X</button>
+                </div>
+            `;
+            
+            const botaoAdicionar = elementoClicado.closest('.add-btn');
+            const caixaScroll = botaoAdicionar.parentElement.querySelector('.container-scroll-horarios');
+            
+            if (caixaScroll) {
+                caixaScroll.insertAdjacentHTML('beforeend', novaLinhaHTML);
+            } else {
+                botaoAdicionar.insertAdjacentHTML('beforebegin', novaLinhaHTML);
             }
         }
     });
-
 });
